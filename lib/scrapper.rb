8d1696dd6a01b62600bcbfe209c477065f45ad22
figo5530 <figooooo0221@gmail.com>
2021-01-30 15:37:06 -0500

    defines two methods in Scrapper class.

---
+++ lib/scrapper.rb
@@ -0,0 +1,14 @@
+require 'open-url'
+require 'nokogiri'
+
+class Scrapper
+
+    def self.scrpae_team_from_league(league_name)
+
+    end
+
+    def self.scrpae_stat_from_league(league_name)
+
+    end
+
+end

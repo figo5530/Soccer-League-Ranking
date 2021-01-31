fd78b7a3b8eb9ad60257864d4a1a318a69eef94d
figo5530 <figooooo0221@gmail.com>
2021-01-31 16:02:02 -0500

    defines and implements #prompt_for_league in CLI class.

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,6 +8,21 @@ class CLI
         self.prompt_for_league
     end
 
+    def prompt_for_league
+        puts "Which league would you like to check?"
+        puts "Please enter ita for Serie A, esp for La Liga, eng for Premier League."
+        input = gets.strip
+        if input == "ita" || input == "esp" || input == "eng"
+            team_array = Scrapper.scrpae_team_from_league("eng")
+            stat_array = Scrapper.scrpae_stat_from_league("eng")
+            Team.create_from_collection(team_array)
+            Team.complete_team_stat(stat_array)
+        else
+            puts "Sorry, your input was invalid or beyond our database."
+            self.prompt_for_league
+        end
+    end
+
     def show_table
         puts " Team  GP  W  D  L  F  A  GD  P"
         Team.all.each {|team| puts "#{team.name} #{team.game_played} #{team.wins} #{team.draws} #{team.losses} #{team.goals_for} #{team.goals_against} #{team.goal_difference} #{team.points}" }

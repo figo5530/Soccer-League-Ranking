1d51780a17ab2227b0c455cf9a4e7e93b4a899cf
figo5530 <figooooo0221@gmail.com>
2021-01-31 00:19:48 -0500

    defines and implements #show_table in Team class.

--- lib/team.rb
+++ lib/team.rb
@@ -27,4 +27,9 @@ class Team
     def self.all
         @@all
     end
+
+    def self.show_table
+        puts " Team  GP W D L F A GD P"
+        all.each {|team| puts "#{team.name} #{team.game_played} #{team.wins} #{team.draws} #{team.losses} #{team.goals_for} #{team.goals_against} #{team.goal_difference} #{team.points}" }
+    end
 end

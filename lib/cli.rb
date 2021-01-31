43b7690e52b574ff35ecd48ba2b9cc410168afab
figo5530 <figooooo0221@gmail.com>
2021-01-31 15:48:31 -0500

    defines and implements #welcome in CLI class.

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,14 +3,21 @@ class CLI
     def initialize
     end
 
+    def welcome
+        puts "Welcome to the Soccer League Ranking app!"
+        self.prompt_for_league
+    end
+
     def show_table
-        puts " Team  GP W D L F A GD P"
+        puts " Team  GP  W  D  L  F  A  GD  P"
         Team.all.each {|team| puts "#{team.name} #{team.game_played} #{team.wins} #{team.draws} #{team.losses} #{team.goals_for} #{team.goals_against} #{team.goal_difference} #{team.points}" }
     end
 
     def test
-        team_array = Scrapper.scrpae_team_from_league("ita")
-        stat_array = Scrapper.scrpae_stat_from_league("ita")
+        # team_array = Scrapper.scrpae_team_from_league("ita")
+        # stat_array = Scrapper.scrpae_stat_from_league("ita")
+        team_array = Scrapper.scrpae_team_from_league("eng")
+        stat_array = Scrapper.scrpae_stat_from_league("eng")
         Team.create_from_collection(team_array)
         Team.complete_team_stat(stat_array)
     end
